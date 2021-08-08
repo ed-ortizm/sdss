681a54b422e0bf50819e3cc45554e9fbf3981bc2
Edgar Ortiz <ed.ortiz@gmail.com>
2021-08-08 17:10:07 -0400

    meta data frame saved correctly

--- raw_spectra_processing.py
+++ raw_spectra_processing.py
@@ -44,4 +44,5 @@ data_processing = RawDataProcessing(galaxies_df=gs,
 data_processing.get_raw_spectra()
 ################################################################################
 # saving data frame with meta data of the raw a spectra in the rest frame
-data_processing.meta_data_frame.to_csv(path_or_buf=output_directory)
+data_processing.meta_data_frame.to_csv(
+    path_or_buf=f'{output_directory}/meta_data_frame.csv')
