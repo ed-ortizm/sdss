b246837b559e9154ef44ce601b4e26e9088f1414
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-21 09:58:40 -0300

    Add sample scripts

---
+++ sample.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+from configparser import ConfigParser, ExtendedInterpolation
+import multiprocessing as mp
+import time
+
+import numpy as np
+import pandas as pd
+
+###############################################################################
+if __name__=="__main__":
+    mp.set_start_method("spawn")
+
+    start_time = time.time()
+    ###########################################################################
+    parser = ConfigParser(interpolation=ExtendedInterpolation())
+    parser.read("sample.ini")
+    ###########################################################################
+    finish_time = time.time()
+    print(f"Run time: {finish_time - start_time:.2f}")
