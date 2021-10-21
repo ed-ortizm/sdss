e8ee649d43db8bf875ce6a30594570ecb1a095d8
Edgar Ortiz <ed.ortizm@gmail.com>
2021-10-21 09:11:45 -0300

    Document class GetRawData

--- src/raw/data.py
+++ src/raw/data.py
@@ -8,21 +8,25 @@ import numpy as np
 import pandas as pd
 
 ###############################################################################
-def init_get_data_worker(input_counter: "mp.Value", in_df) -> "None":
+def init_get_data_worker(
+    input_counter: "mp.Value",
+    input_df: "pandas dataframe"
+    ) -> "None":
     """
     Initialize worker for download
     PARAMETERS
         counter: counts the number of the child process
+        input_df: pandas dataframe to be accessible to each child
     """
     global counter
     global galaxies_df
 
     counter = input_counter
-    galaxies_df = in_df
+    galaxies_df = input_df
 
-################################################################################
+###############################################################################
 class GetRawData:
-    """Handled raw data from sdss"""
+    """Get wave, flux and ivar from sdss dr16 spectra"""
 
     def __init__(
         self,
@@ -33,7 +37,6 @@ class GetRawData:
         """
         PARAMETERS
 
-            galaxies_df : data frame containing meta data from sdss
             data_directory : sdss raw data's directory
             output_directory : save here .npy files
             number_processes : number of processes to use with mp.Pool
@@ -76,21 +79,21 @@ class GetRawData:
 
             results = pool.map(self._get_data, galaxy_indexes)
 
-
         number_fail = sum(results)
         print(f"Fail with {number_fail} files")
 
     ###########################################################################
     def _get_data(self, galaxy_index: "int")-> "int":
         """
-        Gets a row of meta data
+        Save data from spetrum corresponding to galaxy_index in
+        galaxies_df
 
         PARAMETERS
             galaxy_index: index of a galaxy in that galaxy that the
                 frame passed to the constructor of the class
 
         OUTPUT
-            1 if file is not present, 0 otherwise
+            0 for successful operation, 1 otherwise
         """
 
         [
@@ -124,14 +127,22 @@ class GetRawData:
         file_location: "str",
         spectrum_name: "str",
         # number_attempts: "int" = 10
-    )-> "None":
+    )-> "int":
+
         """
+        Get and save wav, flux and ivar
         PARAMETER
+
             galaxy_index: index of the galaxy in the input data frame
                 passed to the constructor
-            file_location: directory location of the fits file
-            spectrum_name:
-            number_attempts:
+            file_location: location of the fits file including extension
+            spectrum_name: name of spectrum
+                spec-{plate}-{mjd}-{fiberid}
+            number_attempts: [no implement yet]
+                how many times to try if warning
+
+        OUTPUT
+            0 for successful operation, 1 otherwise
         """
 
         save_to = f"{self.rest_frame_directory}/{spectrum_name}.npy"
@@ -196,15 +207,14 @@ class GetRawData:
         return [file_directory, spectrum_name]
 
     ###########################################################################
-    def _galaxy_identifiers(self, galaxy: "df.row"):
+    def _galaxy_identifiers(self, galaxy: "df.row")-> "list":
         """
         PARAMETER
             galaxy : pd.row from the galaxy data frame passed to
                 the constructor of the class
 
         OUTPUT
-            return plate, mjd, fiberid, run2d
-
+            return [plate, mjd, fiberid, run2d]
                 plate: self explanatory
                 mjd: date
                 fiberid: self explanatory
@@ -220,7 +230,10 @@ class GetRawData:
         return [plate, mjd, fiberid, run2d]
 
     ###########################################################################
-    def _check_directory(self, directory: "str", exit: "bool" = False):
+    def _check_directory(self,
+        directory: "str",
+        exit: "bool" = False
+    )->"None":
         """
         Check if a directory exists, if not it creates it or
         exits depending on the value of exit
@@ -236,7 +249,7 @@ class GetRawData:
             os.makedirs(directory)
 
     ###########################################################################
-    def _file_exists(self, location: "str", exit: "bool" = False):
+    def _file_exists(self, location: "str", exit: "bool" = False)->"bool":
         """
         Check if a location is a file, if not exits depending
         on the value of exit
@@ -256,6 +269,5 @@ class GetRawData:
             return file_exists
 
         return file_exists
-
 
 ###############################################################################
