f923a75c97f68862a8947a638751ab54fd44c07f
Edgar Ortiz <ed.ortizm@gmail.com>
2021-11-06 14:31:55 -0300

    Implement to_latex method in DataDescription

--- sample/describe.py
+++ sample/describe.py
@@ -1,4 +1,3 @@
-
 #! /usr/bin/env python3
 from configparser import ConfigParser, ExtendedInterpolation
 import multiprocessing as mp
@@ -7,6 +6,7 @@ import time
 import numpy as np
 import pandas as pd
 
+from sdss.describe import DataDescription
 from sdss.process.sample import FileDirectory
 from sdss.process.sample import SampleData
 
@@ -18,8 +18,6 @@ if __name__ == "__main__":
     ###########################################################################
     parser = ConfigParser(interpolation=ExtendedInterpolation())
     parser.read("describe.ini")
-    # Check files and directory
-    check = FileDirectory()
     ###########################################################################
     # get relevant data
     meta_data_directory = parser.get("directories", "meta_data")
@@ -36,7 +34,40 @@ if __name__ == "__main__":
     if number_spectra != -1:
         spectra_df = spectra_df[:number_spectra]
     ###########################################################################
-    data_description = spectra_df[["z", "snMedian"]].describe()
+    str_to_list = lambda x: x.replace(" ", "").split(",")
+    ###########################################################################
+    describe_parameters = parser.get("parameters", "describe")
+    describe_parameters = str_to_list(describe_parameters)
+
+    data_description = spectra_df[describe_parameters].describe()
+    ###########################################################################
+    # Set parameters for memoir table
+    formatter = lambda x: f"{x:.4f}" if x%1 != 0 else f"{x:.0f}"
+
+    save_to = parser.get("latex", "save_to")
+
+    header = parser.get("latex", "header")
+    header = str_to_list(header)
+
+    formatters = [formatter, formatter]
+    caption = parser.get("latex", "caption")
+    label = parser.get("latex", "label")
+    bold_rows = parser.getboolean("latex", "bold_rows")
+    position = parser.get("latex", "position")
+    index = parser.getboolean("latex", "index")
+
+    DataDescription().description_to_latex(
+        data_description=data_description,
+        save_to=save_to,
+        header=header,
+        formatters=formatters,
+        caption=caption,
+        label=label,
+        bold_rows=bold_rows,
+        position=position,
+        index=index,
+    )
+    print(caption)
     ###########################################################################
     finish_time = time.time()
     print(f"Run time: {finish_time - start_time:.2f}")
--- src/sdss/describe.py
+++ src/sdss/describe.py
@@ -5,33 +5,16 @@ class DataDescription:
         pass
 
     def description_to_latex(self,
-        data_description: pandas.DataFrame,
+        data_description: pd.DataFrame,
         save_to: str,
         header: list,
         formatters: list,
         caption: tuple,
         label: str,
         bold_rows: bool=True,
-        position: str="!ht"
+        position: str="!ht",
         index: bool=True,
     )-> None:
-        """
-        Converts descriptive statistics of pandas to a latex table
-        that uses \usepackage{booktabs} to be properly processed
-
-        PARAMETERS
-            data_description:
-            save_to:
-            header:
-            index:
-            formatters:
-            caption:
-            label:
-            bold_rows:
-            position:
-
-        """
-
         data_description.to_latex(
             buf=save_to,
             header=header,
@@ -43,3 +26,19 @@ class DataDescription:
             index=index
         )
 ###############################################################################
+        # """
+        # Converts descriptive statistics of pandas to a latex table
+        # that uses \usepackage{booktabs} to be properly processed
+
+       #  PARAMETERS
+        #     data_description:
+        #     save_to:
+        #     header:
+        #     index:
+        #     formatters:
+        #     caption:
+        #     label:
+        #     bold_rows:
+        #     position:
+
+       #  """
