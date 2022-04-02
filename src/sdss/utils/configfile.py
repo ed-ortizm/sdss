210dfd27bfd6801e2ad92a148951552579a009e3
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-04-02 12:52:28 -0300

    Add utils module to handle .ini and files in general. Add metadata module rather than having it embedded in a generic module

---
+++ src/sdss/utils/configfile.py
@@ -0,0 +1,139 @@
+###############################################################################
+class ConfigurationFile:
+    """Manage common operation with configuration files.
+    For instance:
+
+        [parameters]
+        metric = mse, lp, mad
+
+    when reading metric I can inmediately get it as a list if looping
+    is needed in addition, I can get a whole section in the configuration
+    file as a dictionary and do the necessary transformation to each key,
+    value pair
+
+    """
+
+    def __init__(self):
+        pass
+
+    ###########################################################################
+    def section_to_dictionary(
+        self,
+        section_items: tuple,
+        value_separators: list,
+        # multiple_lines: bool,
+        # comma_values:bool,
+    ) -> dict:
+        """
+        Converts a section in the configuration file to a dictionary.
+        WARNING: all values are strings.
+        If there is a variable with multiple lines, the values of the
+        associated key in the dictionary would be a list
+
+        PARAMETERS
+            section_items: items in a section of the configuration file
+
+        OUTPUTS
+            section_dictionary: items transformed
+        """
+        section_dictionary = dict(section_items)
+
+        for key, value in section_dictionary.items():
+
+            value = str(value)
+            ###################################################################
+            if "\n" in value:
+
+                value = value.split("\n")
+                section_dictionary[key] = value
+
+            ###################################################################
+            for separator in value_separators:
+
+                if separator in value:
+
+                    value = value.split(separator)
+                    section_dictionary[key] = value
+            ###################################################################
+
+        section_dictionary = self._transform_values_in_dictionary(
+            section_dictionary
+        )
+
+        return section_dictionary
+
+    ###########################################################################
+    def entry_to_list(self, entry: str, entry_type: type, separator: str)-> list:
+        """
+
+        PARAMETERS
+
+            entry: a coma separated string
+                architecture: 100, 50, 5, 50, 100
+
+            entry_type: either str, float, int or bool
+            separator: separator to use when spliting entry string
+
+        OUTPUTS
+            entry_list: list of elements in entry with the type
+                100 separator 50 separator 5... --> [100, 50, 5, ...]
+        """
+        entry = entry.strip().split(separator)
+        entry = [entry_type(value) for value in entry]
+
+        return entry
+    ###########################################################################
+    def _transform_values_in_dictionary(self, dictionary: dict):
+
+        for key, value in dictionary.items():
+
+            is_list = type(value) is list
+            value = self._transform_values(value, is_list)
+
+            dictionary[key] = value
+
+        return dictionary
+
+    ###########################################################################
+    def _transform_values(self, items: str, is_list: bool = False):
+
+        if is_list is True:
+
+            new_items = []
+
+            for string in items:
+
+                value = self._get_value_from_string(string)
+                new_items.append(value)
+
+            return new_items
+
+        return self._get_value_from_string(items)
+
+    ###########################################################################
+    def _get_value_from_string(self, string: str):
+
+        """
+        Get value from string variable, could be: bool, str, int or float
+        """
+        string = string.strip()
+        #######################################################################
+        if (string == "True") or (string == "False"):
+
+            return string == "True"
+
+        #######################################################################
+        if (string.isalpha() is True) | ("_" in string) is True:
+
+            return string
+        #######################################################################
+        numeric_value = float(string)
+        if numeric_value.is_integer():
+
+            return int(numeric_value)
+
+        return numeric_value
+
+    ###########################################################################
+
+    ###########################################################################
---
+++ src/sdss/utils/managefiles.py
@@ -0,0 +1,63 @@
+class FileDirectory:
+    """Handle common operations with files and directories"""
+
+    def __init__(self):
+        pass
+
+    ###########################################################################
+    def check_directory(self, directory: str, exit: bool = False) -> "None":
+        """
+        Check if a directory exists, if not it creates it or
+        exits depending on the value of exit
+        """
+
+        if not os.path.exists(directory):
+
+            if exit:
+                print(f"Directory {directory} NOT FOUND")
+                print("Code cannot execute")
+                sys.exit()
+
+            os.makedirs(directory)
+
+    ###########################################################################
+    def file_exists(self, location: str, exit: bool = False) -> bool:
+        """
+        Check if a location is a file, if not exits depending
+        on the value of exit
+        """
+
+        file_exists = os.path.isfile(location)
+
+        if not file_exists:
+
+            file_name = location.split("/")[-1]
+
+            if exit:
+                print(f"File {file_name} NOT FOUND!")
+                print("Code cannot execute")
+                sys.exit()
+
+            return file_exists
+
+        return file_exists
+
+    ###########################################################################
+    def remove_file(self, file_location: str) -> "None":
+
+        """
+        remove file at file_location
+        PARAMETERS
+            file_location: e.g. "/home/user/file.text"
+        """
+
+        file_name = file_location.split("/")[-1]
+
+        if not self.file_exists(file_location, exit=False):
+
+            print(file_location)
+            print(f"There is no {file_name}!", end="\r")
+
+        else:
+            os.remove(file_location)
+            print(f"File {file_name} removed!", end="\r")
