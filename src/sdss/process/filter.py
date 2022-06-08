f86401f5c08ad98734cf674e0d04803106ade041
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-06-08 10:25:07 -0400

    fetarure: implement filter to remove noise on spectra

--- src/sdss/process/filter.py
+++ src/sdss/process/filter.py
@@ -2,13 +2,24 @@
 from astropy.convolution import Gaussian1DKernel, convolve
 import numpy as np
 
-def filter_noise(spectrum, kernel_size):
-    """Filter noise in spectra"""
+def filter_noise(spectrum: np.array, kernel_size: int) -> tuple:
+    """
+    Filter noise on a spectrum with a gaussian kernel
+
+    INPUT
+
+    spectrum: data with noise
+    kernel_size: number of elements in gaussian kernel
+
+    OUTPUT
+
+    filtered_spectrum, noise:
+        filtered_spectrum: spectrum with noise removed
+        noise: spectrum's noise 
+    """
     kernel = Gaussian1DKernel(kernel_size)
 
-    extended_spectrum = np.hstack((spectrum[:10], spectrum, spectrum[-10:]))
-    extended_spectrum = convolve(extended_spectrum, kernel)
-    filtered_spectrum = extended_spectrum[10:-10]
+    filtered_spectrum = convolve(spectrum, kernel, boundary="extend")
     noise = spectrum - filtered_spectrum
 
-    return noise, filtered_spectrum
+    return filtered_spectrum, noise
