51e828927901df013b04e2b337cae72c92388f8f
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-11-14 09:55:28 -0300

    explore: add notebook to inspect spectra binning according to SNR

--- cas_queries/meta_data_query.sql
+++ cas_queries/meta_data_query.sql
@@ -1,25 +1,25 @@
 SELECT
-  
-  specobjid,  
-  mjd,  
-  plate,  
-  fiberid,  
-  run2d,  
-  ra,  
-  dec,  
-  z,  
-  zErr,  
-  zWarning,  
-  class,  
-  subClass,  
-  z_noqso,  
+
+  specobjid,
+  mjd,
+  plate,
+  fiberid,
+  run2d,
+  ra,
+  dec,
+  z,
+  zErr,
+  zWarning,
+  class,
+  subClass,
+  z_noqso,
   zErr_noqso,
   zWarning_noqso,
   targetType,
   programname,
-  instrument,  
+  instrument,
   snMedian
-  
+
 FROM SpecObj
 WHERE survey='sdss'
 ORDER BY snMedian DESC
