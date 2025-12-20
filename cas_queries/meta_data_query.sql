555a3b626d6db54e7181ca1bcccb64a663642d8d
Edgar Ortiz <ed.ortizm@gmail.com>
2025-12-20 07:49:50 -0300

    chore: update sql format to get sdss metadata

--- cas_queries/meta_data_query.sql
+++ cas_queries/meta_data_query.sql
@@ -1,25 +1,14 @@
 SELECT
-
   specobjid,
-  mjd,
-  plate,
-  fiberid,
-  run2d,
-  ra,
-  dec,
-  z,
-  zErr,
-  zWarning,
-  class,
-  subClass,
-  z_noqso,
-  zErr_noqso,
-  zWarning_noqso,
+  mjd, plate, fiberid, run2d,
+  ra, dec,
+  snMedian,
+  z, zErr, zWarning,
+  z_noqso, zErr_noqso, zWarning_noqso,
+  class, subClass,
   targetType,
   programname,
-  instrument,
-  snMedian
-
-FROM SpecObj
+  instrument
+FROM SpecObj
 WHERE survey='sdss'
 ORDER BY snMedian DESC
